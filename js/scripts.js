388e996f4ceb073691f79fee87fc48f1649eb71c
Michał Gacek <gacek.michael@outlook.com>
2017-04-12 22:19:22 +0200

    Fix

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,11 @@
 $(function(){
 	var carouselList = $('#carousel ul'),
 		prevBtn = $('#prevbtn'),
-		nextBtn = $('#nextbtn');
+		nextBtn = $('#nextbtn'),
+		interval;
 	
 //interval
-	setInterval(function changeSlide() {
-		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);	
-	}, 2500);
+	interval = setInterval(changeOneSlideFwd, 2500);
 	
 	function moveFirstSlide() {
 		var firstItem = carouselList.find('li:first'),
@@ -17,20 +16,18 @@ $(function(){
 	};
 	prevBtn.on('click', changeOneSlidePrev);
 	
-	function changeOneSlidePrev() {
+	function changeOneSlidePrev() {		
 		var firstItem = carouselList.find('li:first'),
 			lastItem = carouselList.find('li:last');
-			firstItem.before(lastItem);
-			carouselList.css({marginRight: 0});
-			carouselList.animate({'marginRight':400}, 500);
+
+		clearInterval(interval);	
+		firstItem.before(lastItem);
+		carouselList.css({marginLeft: -400});
+		carouselList.animate({marginLeft: 0}, 500);
 	};
 	nextBtn.on('click', changeOneSlideFwd);
 	
 	function changeOneSlideFwd() {
-		var firstItem = carouselList.find('li:first'),
-			lastItem = carouselList.find('li:last');
-			lastItem.after(firstItem);
-			carouselList.css({marginLeft: 0});
-			carouselList.animate({'marginLeft':-400}, 500);
+		carouselList.animate({marginLeft: -400}, 500, moveFirstSlide);
 	};
 });
